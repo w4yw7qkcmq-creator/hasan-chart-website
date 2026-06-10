142c446c826275f65509bfc5e8c96642a7ab4537
Hasan <msjdudieneign@gmail.com>
2026-06-11 01:00:53 +0300

    add second telegram source and improve rewrite rules

--- worker/news-worker.js
+++ worker/news-worker.js
@@ -713,6 +713,12 @@ const TELEGRAM_SOURCE_CHANNELS = [
   {
     name: "ForexBreakingNews",
     url: "https://t.me/s/ForexBreakingNews",
+    priority: 1,
+  },
+  {
+    name: "ForexNewspaper",
+    url: "https://t.me/s/ForexNewspaper",
+    priority: 2,
   },
 ];
 
