22e2937df8e733f1ed86e2d38e6239e0081fde57
Hasan <msjdudieneign@gmail.com>
2026-06-14 17:45:18 +0300

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,32 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.hasanchartworld.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.hasanchartworld.com/news",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.hasanchartworld.com/daily-analysis",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.hasanchartworld.com/subscriptions",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.hasanchartworld.com/affiliate",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.hasanchartworld.com/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.hasanchartworld.com/register",
+      lastModified: new Date(),
+    },
+  ];
+}
