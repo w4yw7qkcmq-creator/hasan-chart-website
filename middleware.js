7f8a1cb4b9ad5497132080c2b1d66b9286b603d3
Hasan <msjdudieneign@gmail.com>
2026-06-19 14:48:42 +0300

    Add security middleware

---
+++ middleware.js
@@ -0,0 +1,18 @@
+
+
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const response = NextResponse.next();
+
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+
+  return response;
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
