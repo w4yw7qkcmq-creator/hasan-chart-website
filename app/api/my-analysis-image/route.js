171bb174e846d4bfd3dc686ca42fc7e64a8d6dba
Hasan <msjdudieneign@gmail.com>
2026-06-21 15:11:03 +0300

    Fix supabase admin import

--- app/api/my-analysis-image/route.js
+++ app/api/my-analysis-image/route.js
@@ -1,7 +1,7 @@
 
 
 import { NextResponse } from "next/server";
-import { getSupabaseAdmin } from "@/lib/supabase-admin";
+import { getSupabaseAdmin } from "../../../lib/supabase-admin";
 
 const normalizeEmail = (value) =>
   String(value || "")
